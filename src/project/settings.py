dde508177db2751416dbabbe2147d0a0b5048e40
Anastasia Taranova <anastasiataranova@MacBook-Pro-Anastasia.local>
2020-04-06 20:33:00 +0300

    15

--- src/project/settings.py
+++ src/project/settings.py
@@ -21,7 +21,7 @@ REPO_DIR = BASE_DIR.parent.resolve()
 SECRET_KEY = '0)pf!brx%99+5-frzl=h^-*vc4!_rt4wxht7vex1^zjq=dokyq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 0
 
 ALLOWED_HOSTS = [
 "127.0.0.1",
