708846c09a651b368a9fc1ada4df47b4c000ff2a
Anastasia Taranova <anastasiataranova@MacBook-Pro-Anastasia.local>
2020-04-06 19:12:15 +0300

    11

--- src/project/settings.py
+++ src/project/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            PROJECT_DIR / "tempates"
+            PROJECT_DIR / "templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- src/project/urls.py
+++ src/project/urls.py
@@ -2,49 +2,26 @@ from pathlib import Path
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
-#from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render
 
 here = Path(__file__).parent.resolve()
 
 
-def view(r):
-    index = here.parent.parent / "index.html"
-    with index.open() as f:
-        return HttpResponse(f.read())
+def view(request: HttpRequest) -> HttpResponse:
+    return render(request, "index.html")
 
 
-def view_css(r):
-    index = here.parent.parent / "styles.css"
-    with index.open() as f:
-        return HttpResponse(f.read(), content_type="text/css")
+def view_css(request: HttpRequest) -> HttpResponse:
+    return render(request, "styles.css", content_type="text/css")
 
 
-def view_js(r):
-    index = here.parent.parent / "html5shiv.js"
-    with index.open() as f:
-        return HttpResponse(f.read(), content_type="application/js")
+def view_js(request: HttpRequest) -> HttpResponse:
+    return render(request, "html5shiv.js", content_type="application/js")
 
 
-def view_jpg1(r):
-    index = here.parent.parent / "imgs/nastya1.jpg"
-    with index.open("rb") as f:
-        return HttpResponse(f.read())
-
-
-# def view(request):
-# return render(request, "index.html")
-
-
-# def view_jpg1(request):
-# return render(request, "imgs/nastya1.jpg", content_type="image/jpg")
-
-
-# def view_css(request):
-# return render(request, "styles.css", content_type="text/css")
-
-
-# def view_js(request):
-# return render(request, "html5shiv.js", content_type="application/js")
+def view_jpg1(request: HttpRequest) -> HttpResponse:
+    return render(request, "imgs/nastya1.jpg", content_type="image/jpg")
 
 
 urlpatterns = [
