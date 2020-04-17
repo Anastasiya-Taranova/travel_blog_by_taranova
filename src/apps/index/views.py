4ef8f51c599120f156bdc6881566498fb67af5c6
Anastasia Taranova <anastasiataranova@MacBook-Pro-Anastasia.local>
2020-04-17 22:00:15 +0300

    26

--- src/apps/index/views.py
+++ src/apps/index/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from apps.index.models import TripInfo
+
 
 class IndexView(TemplateView):
     template_name = "index/index.html"
+
+    def get_context_data(self, **kwargs):
+        parent_ctx = super().get_context_data(**kwargs)
+        info = TripInfo.objects.first()
+        ctx = {"country": info.country}
+        ctx.update(parent_ctx)
+        return ctx
