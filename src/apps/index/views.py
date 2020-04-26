5c41973c4703dc5d771c4f9a049f5699c9a78359
anastasiataranova <taranova.nas@gmail.com>
2020-04-26 13:46:54 +0300

    correct models

--- src/apps/index/views.py
+++ src/apps/index/views.py
@@ -1,14 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from apps.index.models import TripInfo
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "index/index.html"
-
-    def get_context_data(self, **kwargs):
-        parent_ctx = super().get_context_data(**kwargs)
-        info = TripInfo.objects.first()
-        ctx = {"country": info.country}
-        ctx.update(parent_ctx)
-        return ctx
+    model = TripInfo
