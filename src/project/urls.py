5ae74f074df19e80865ffed5a419b93a4f3e64ca
Anastasiya Taranova <48852789+Anastasiya-Taranova@users.noreply.github.com>
2020-04-26 13:47:45 +0300

    Second (#3)
    * Update
    
    * 11111
    
    * correct models

--- src/project/urls.py
+++ src/project/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from django.urls import path, include
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("apps.index.urls")),
-    path('vietnam/', include('apps.vietnam.urls'))]
+    path('vietnam/', include('apps.vietnam.urls')),
+    path('sentry-debug/', trigger_error), ]
