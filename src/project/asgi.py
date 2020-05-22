8acb7ffacfb4c9dcb4c7b5fe8970458be4614328
anastasiataranova <taranova.nas@gmail.com>
2020-05-22 13:38:29 +0300

    A

--- src/project/asgi.py
+++ src/project/asgi.py
@@ -4,4 +4,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-application = get_asgi_application()
+apps = get_asgi_application()
