5f36c765dd3caf8a6f6e0f9544c3dca4d6d6b90e
anastasiataranova <taranova.nas@gmail.com>
2020-06-04 00:15:55 +0300

    1

--- src/apps/api/impl/v1/serializers.py
+++ src/apps/api/impl/v1/serializers.py
@@ -8,4 +8,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = AuthProfile
         fields = [a(_f) for _f in (AuthProfile.verification_code, AuthProfile.user)]
-        read_only_fields = [a(_f) for _f in (AuthProfile.verification_code, AuthProfile.verified_at,)]
+        read_only_fields = [
+            a(_f) for _f in (AuthProfile.verification_code, AuthProfile.verified_at,)
+        ]
