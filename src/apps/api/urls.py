fb35e4329a3950c3c33765c8d639cf86200bc9d1
anastasiataranova <taranova.nas@gmail.com>
2020-06-13 15:48:58 +0300

    weather app

--- src/apps/api/urls.py
+++ src/apps/api/urls.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.urls import include
 from django.urls import path
 from django.urls import re_path
+from django.views.decorators.csrf import csrf_exempt
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
 
 # TODO: move to views
 from apps.api.views import ObtainAuthToken
+from apps.api.views import TelegramView
 
 schema_view = get_schema_view(
     openapi.Info(
---
+++ src/apps/weather/views.py
@@ -0,0 +1,34 @@
+import requests
+from django.shortcuts import render
+
+from apps.weather.forms import CityForm
+from apps.weather.models import City
+
+
+def index(request):
+    appid = "b4be221bb02fa8a7be1d57407c585b5b"
+    url = "http://api.openweathermap.org/data/2.5/find?q={}&units=metric&appid=" + appid
+
+    if (request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"][0]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form': form}
+
+    return render(request, 'weather/index.html', context)
