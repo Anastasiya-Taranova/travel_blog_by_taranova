051c57e9d51e5d0afd9d9bca1d9b063ec06522dd
Anastasiya Taranava <taranova.nas@gmail.com>
2020-09-22 19:19:09 +0300

    detect-venv

--- detect_venv.py
+++ detect_venv.py
@@ -3,36 +3,33 @@ import sys
 
 
 def get_dynaconf_env():
-    env_var = "ENV_FOR_DYNACONF"
-
+    var_name = "ENV_FOR_DYNACONF"
+    default_env = "development"
     try:
         from dynaconf import settings
-        env = settings.get(env_var)
+        env = settings.get(var_name)
     except ImportError:
-        env = os.getenv(env_var)
-
-    return env or "development"
+        env = os.getenv(var_name)
+    return env or default_env
 
 
 def get_base_prefix_compat():
     """
     Get base/real prefix, or sys.prefix if there is none.
     """
-
     prefix = (
             getattr(sys, "base_prefix", None)
             or getattr(sys, "real_prefix", None)
             or sys.prefix
     )
-
     return prefix
 
 
 def in_virtualenv():
-    dynaconf_env = get_dynaconf_env()
-    if dynaconf_env == "heroku":
+    env = get_dynaconf_env()
+    if env == "heroku":
         return True
     return get_base_prefix_compat() != sys.prefix
 
 
-
+print(in_virtualenv())
